7e350a03240c263ed4a495d313f4ba7535dab164
caolong <caolong.bj02@bytedance.com>
2020-08-26 20:24:34 +0800

    Initial Commit

---
+++ OpenGLCoding02/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OpenGLCoding02
+//
+//  Created by 曹龙 on 2020/8/26.
+//  Copyright © 2020 曹龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ OpenGLCoding02/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  OpenGLCoding02
+//
+//  Created by 曹龙 on 2020/8/26.
+//  Copyright © 2020 曹龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
